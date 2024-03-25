1d8466093a3dbbcad47487873eae002dbfec58e4
raycapmbmgc <139500594+raycapmbmgc@users.noreply.github.com>
2024-03-25 16:11:23 -0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,15 @@
 const db = require('../public/db');
 
-function getUserByEmail(email, callback) {
+function getUserByEmail(email, callback) { //Esta função recebe um email como entrada e retorna o usuário correspondente a esse email, se existir.
   db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
     if (err) {
       return callback(err, null);
     }
-    callback(null, results[0]);
+    callback(null, results[0]); //Retorna o usuário encontrado ou um erro, através de uma função de retorno de chamada.
   });
 }
 
-function createUser(email, password, callback) {
+function createUser(email, password, callback) { //Esta função recebe um email e uma senha como entrada e cria um novo usuário com esses dados.
   db.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, password], (err, results) => {
     if (err) {
       return callback(err);
@@ -22,3 +22,9 @@ module.exports = {
   getUserByEmail,
   createUser
 };
+//As funções getUserByEmail e createUser são exportadas como um objeto para que outras partes do código possam utilizá-las para interagir com o banco de dados.
+
+
+
+
+
